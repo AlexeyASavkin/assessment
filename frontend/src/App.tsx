240a0313640ad988d73d31188b92fe041a0d9c28
Алексей Савкин <savkin.alexey@gmail.com>
2026-07-26 18:10:54 +0300

    refactor: удаление раздела «Токены» из админки
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ import AdminLogin from './pages/admin/AdminLogin'
 import AdminDashboard from './pages/admin/AdminDashboard'
 import AdminCompetenciesTreePage from './pages/admin/AdminCompetenciesTreePage'
 import EmployeesPage from './pages/admin/EmployeesPage'
-import TokensPage from './pages/admin/TokensPage'
 import ApplicationsPage from './pages/admin/ApplicationsPage'
 import ApplicationReportPage from './pages/admin/ApplicationReportPage'
 import AiSettingsPage from './pages/admin/AiSettingsPage'
@@ -49,7 +48,6 @@ function App() {
               <Route path="/admin/competencies" element={<AdminCompetenciesTreePage />} />
               <Route path="/admin/competencies/:competencyId/questions" element={<QuestionsPage />} />
               <Route path="/admin/employees" element={<EmployeesPage />} />
-              <Route path="/admin/tokens" element={<TokensPage />} />
               <Route path="/admin/applications" element={<ApplicationsPage />} />
               <Route path="/admin/applications/:sessionId/report" element={<ApplicationReportPage />} />
               <Route path="/admin/settings" element={<AiSettingsPage />} />
